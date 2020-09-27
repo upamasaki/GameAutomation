8c56d50e35391be2145d14636991269bbc3197d9
upamasaki <m.tani.july@gmail.com>
2020-09-27 21:12:43 +0900

    add image

--- main.py
+++ main.py
@@ -15,11 +15,19 @@ def uchu_main(GameAuto):
     while 1:
         print(">>>>>>>>> uchu >>>>>>")
         GameAuto.serch_click_image2('./img/uchu/start.png', 1, 0.8)
-        GameAuto.serch_click_image2('./img/uchu/close.png', 1, 0.7)
-        GameAuto.serch_click_image2('./img/uchu/close2.png', 1, 0.7)
+        # GameAuto.serch_click_image2('./img/uchu/close.png', 1, 0.7)
+        # GameAuto.serch_click_image2('./img/uchu/close2.png', 1, 0.7)
         GameAuto.serch_click_image2('./img/uchu/close3.png', 1, 0.8)
+        GameAuto.serch_click_image2('./img/uchu/close4.png', 1, 0.8)
+        GameAuto.serch_click_image2('./img/uchu/close5.png', 1, 0.8)
+        GameAuto.serch_click_image2('./img/uchu/close6.png', 1, 0.8)
+        GameAuto.serch_click_image2('./img/uchu/close7.png', 1, 0.8)
+        GameAuto.serch_click_image2('./img/uchu/close8.png', 1, 0.8)
+        GameAuto.serch_click_image2('./img/uchu/close9.png', 1, 0.8)
+        GameAuto.serch_click_image2('./img/uchu/close10.png', 1, 0.8)
+        GameAuto.serch_click_image2('./img/uchu/close11.png', 1, 0.9)
         GameAuto.serch_click_image2('./img/uchu/retry.png', 1, 0.8)
-
+        
         time.sleep(2)
 
 #以下、メインルーチン
@@ -33,5 +41,6 @@ if __name__ == "__main__":
     print("=========================")
 
         
+
 
 
