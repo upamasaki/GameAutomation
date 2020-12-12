72fcd4d2bdde3905ddb5b69c5573ff0de70bcc01
upamasaki <m.tani.july@gmail.com>
2020-12-13 00:35:03 +0900

    add youjyo_senki

--- main.py
+++ main.py
@@ -11,6 +11,25 @@ import time
 
 count = 0
 
+def temp(GameAuto):
+    counter = 0
+    item_get_count = 10
+    start = time.time()
+    elapsed_time = -1
+    
+    while 1:
+        eval_mode = True
+        
+        ###############################################
+        # 自動化のコードだよ
+        #
+        GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/order_flag2.PNG', wait_time=1, conf=0.9, offset=(20, 20), return_loc=eval_mode)
+        GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/item_get_v2.PNG', wait_time=1, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+        GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/close3.PNG', wait_time=1, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+        GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/ok6.PNG', wait_time=1, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+        
+
+
 def uchu_main(GameAuto):
     while 1:
         print(">>>>>>>>> uchu >>>>>>")
@@ -83,28 +102,116 @@ def kyoshin_seijo_event_humetsu(GameAuto):
 
         time.sleep(2)
 
-
 def kyoshin_seijo_event_oujya(GameAuto):
     while 1:
         eval_mode = True
         
+        # ###############################################
+        # # オーダーの獲得
+        # #
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/order_v3.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/order_v3.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/item_get_v3.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/ok8.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/close4.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+
+        # ###############################################
+        # # 通常の戦闘
+        # #
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/battle7.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/battle8.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/ok7.PNG', wait_time=1, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/come_back_v3.PNG', wait_time=3, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+
+
+        # ###############################################
+        # # イベントアイテムの交換
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/event_item.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/oujya_soshina.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/item_plus.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/item_plus.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/item_plus.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/item_plus.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/item_plus.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/item_exchange.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/ok7.PNG', wait_time=2, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/item_get_close.PNG', wait_time=2, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+        
+        # ###############################################
+        # # 探索グリフのセット
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/glyph_set.PNG', wait_time=1, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/glyph_clear.PNG', wait_time=1, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/glyph_recomm.PNG', wait_time=1, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/glyph_back.PNG', wait_time=1, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+
+        # ###############################################
+        # # 勝手に探索
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/battle7.PNG', wait_time=1, conf=0.8, offset=(-200, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/ok7.PNG', wait_time=2, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+
         ###############################################
-        # 通常の戦闘
-        #
-        GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/battle7.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
-        GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/battle8.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
-        GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/come_back_v3.PNG', wait_time=3, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+        # 託すを発動
+        GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/takusu_v2.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+        GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/takusu_v2.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+        GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/takusu_v2.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+        GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/takusu_v2.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
 
 
 
+        time.sleep(1)
+
+def DOA_XVV(GameAuto):
+    while 1:
+        eval_mode = True
+        GameAuto.serch_click_image2(img_path='./img/doaxvv/start_v1.PNG', wait_time=0, conf=0.7, offset=(100, 0), return_loc=eval_mode)
+        GameAuto.serch_click_image2(img_path='./img/doaxvv/start_v1.PNG', wait_time=0, conf=0.7, offset=(100, 0), return_loc=eval_mode)
+        GameAuto.serch_click_image2(img_path='./img/doaxvv/start_v1.PNG', wait_time=0, conf=0.7, offset=(100, 0), return_loc=eval_mode)
+        GameAuto.serch_click_image2(img_path='./img/doaxvv/start_v2.PNG', wait_time=0, conf=0.7, offset=(100, 0), return_loc=eval_mode)
+        GameAuto.serch_click_image2(img_path='./img/doaxvv/start_v2.PNG', wait_time=0, conf=0.7, offset=(100, 0), return_loc=eval_mode)
+        GameAuto.serch_click_image2(img_path='./img/doaxvv/skip.PNG', wait_time=0, conf=0.7, offset=(0, -20), return_loc=eval_mode)
+        GameAuto.serch_click_image2(img_path='./img/doaxvv/skip.PNG', wait_time=0, conf=0.7, offset=(0, -20), return_loc=eval_mode)
+        GameAuto.serch_click_image2(img_path='./img/doaxvv/result.PNG', wait_time=0, conf=0.7, offset=(0, -10), return_loc=eval_mode)
+        GameAuto.serch_click_image2(img_path='./img/doaxvv/event_Arank.PNG', wait_time=0, conf=0.7, offset=(100, 0), return_loc=eval_mode)
+        time.sleep(2)
+
+
+
+def youjyo_senki(GameAuto):
+    counter = 0
+    item_get_count = 10
+    start = time.time()
+    elapsed_time = -1
+    
+    while 1:
+        eval_mode = True
+        
         ###############################################
-        # オーダーの獲得
-        #
-        GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/order_v3.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
-        GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/order_v3.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
-        GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/item_get_v3.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
-        GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/ok8.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
-        GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/close4.PNG', wait_time=1, conf=0.8, offset=(0, 0), return_loc=eval_mode)
+        # 書類整理
+        #        
+        if(GameAuto.serch_click_image2(img_path='./img/youjyo/syorui_count.PNG', wait_time=0, conf=0.9, offset=(0, 0), return_loc=eval_mode)):
+            GameAuto.serch_click_image2(img_path='./img/youjyo/syorui_v0.PNG', wait_time=0, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+            GameAuto.serch_click_image2(img_path='./img/youjyo/syorui_v1.PNG', wait_time=0, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+            GameAuto.serch_click_image2(img_path='./img/youjyo/syorui_v2.PNG', wait_time=0, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+            GameAuto.serch_click_image2(img_path='./img/youjyo/syorui_v3.PNG', wait_time=0, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+            GameAuto.serch_click_image2(img_path='./img/youjyo/syorui_v4.PNG', wait_time=0, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+            GameAuto.serch_click_image2(img_path='./img/youjyo/syorui_v5.PNG', wait_time=0, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+            GameAuto.serch_click_image2(img_path='./img/youjyo/syorui_v6.PNG', wait_time=0, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+            GameAuto.serch_click_image2(img_path='./img/youjyo/syorui_close.PNG', wait_time=1, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+        else:
+            GameAuto.serch_click_image2(img_path='./img/youjyo/syorui.PNG', wait_time=1, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+            GameAuto.serch_click_image2(img_path='./img/youjyo/syorui_ok.PNG', wait_time=1, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+    
+
+            time.sleep(1)
+        # raise
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/item_get_v2.PNG', wait_time=1, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/close3.PNG', wait_time=1, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+        # GameAuto.serch_click_image2(img_path='./img/kyoshintoseijyo/ok6.PNG', wait_time=1, conf=0.9, offset=(0, 0), return_loc=eval_mode)
+        
+
+
+
+
 
 #以下、メインルーチン
 if __name__ == "__main__":
@@ -115,7 +222,9 @@ if __name__ == "__main__":
     # sample2(GameAuto)
     # final_gear(GameAuto)
     # kyoshin_seijo_event_humetsu(GameAuto)
-    kyoshin_seijo_event_oujya(GameAuto)
+    # kyoshin_seijo_event_oujya(GameAuto)
+    youjyo_senki(GameAuto)
+    # DOA_XVV(GameAuto)
     print("=========================")
 
         
